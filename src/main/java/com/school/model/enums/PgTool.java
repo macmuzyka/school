9a6cf359c20701836265ec72631bb3b82fcf7fa8
macmuzyka <raweshau@gmail.com>
2025-01-08 10:48:40 +0100

    frontend for manually getting database .dump backup file and restoring database via .dump file input scheduled database backup every Saturday midnight minor changes and improvements with elevating classes & fields to abstract layer and interfaces changed spring servlet path

--- src/main/java/com/school/model/enums/PgTool.java
+++ src/main/java/com/school/model/enums/PgTool.java
@@ -1,9 +1,6 @@
 package com.school.model.enums;
 
 public enum PgTool {
-    pg_restore("pg_restore"),
-    pg_dump("pg_dump");
-
-    PgTool(final String pgDump) {
-    }
+    pg_restore,
+    pg_dump
 }
