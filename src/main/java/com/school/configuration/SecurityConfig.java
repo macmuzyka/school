7a05635ec40fab52acf62874b0f555b1a81685af
macmuzyka <raweshau@gmail.com>
2025-10-05 00:36:49 +0200

    stateful authentication changes

--- src/main/java/com/school/configuration/SecurityConfig.java
+++ src/main/java/com/school/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +21,12 @@ public class SecurityConfig {
                 .oauth2Login(Customizer.withDefaults()); // Keycloak will handle login
 
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return converter;
     }
 }
