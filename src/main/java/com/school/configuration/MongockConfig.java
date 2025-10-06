154adfd6e3314fc99ca80c3df3e80a03f15ac0a7
macmuzyka <raweshau@gmail.com>
2025-10-06 18:16:07 +0200

    added mongodb to a project with StudentDocument object and simple migration with Mongock minor changes to directories to suit separating mongodb repository from jpa repositories

---
+++ src/main/java/com/school/configuration/MongockConfig.java
@@ -0,0 +1,26 @@
+package com.school.configuration;
+
+import io.mongock.driver.mongodb.springdata.v4.SpringDataMongoV4Driver;
+import io.mongock.runner.springboot.MongockSpringboot;
+import io.mongock.runner.springboot.base.MongockInitializingBeanRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Raweshau
+ * on 06.10.2025
+ */
+
+@Configuration
+class MongockConfig {
+    @Bean
+    public MongockInitializingBeanRunner mongockInitializingBeanRunner(SpringDataMongoV4Driver driver,
+                                                                       ApplicationContext context) {
+        return MongockSpringboot.builder()
+                .setDriver(driver)
+                .setSpringContext(context) // must inject ApplicationContext
+                .addMigrationScanPackage("com.school.service.mongodbmigration") // package containing @ChangeUnit classes
+                .buildInitializingBeanRunner();
+    }
+}
